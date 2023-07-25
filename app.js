7beb1a71e9e4b2e769ad14e0bccf3986e0904da2
Arthur Porto <artfporto@gmail.com>
2023-07-25 13:46:09 -0300

    resposta via json

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const express = require('express')      // Requisição do pacote do express
-const app = express()                   // Instancia o Express
-const port = 3000                       // Define a porta
+const express = require("express"); // Requisição do pacote do express
+const app = express(); // Instancia o Express
+const port = 3000; // Define a porta
 
-app.get('/', (req, res) => {            // Cria a rota da raiz do projeto
-    res.send('Seu_nome_completo')
-    console.log('Rota / solicitada')    // Substitua pelo seu nome
-})
+app.get("/", (req, res) => {
+  // Cria a rota da raiz do projeto
+  res.json({
+    nome: "Seu_nome_completo", // Substitua pelo seu nome
+  });
+  console.log("Rota / solicitada");
+});
 
-app.listen(port, () => {                // Um socket para "escutar" as requisições
-    console.log(`Serviço escutando na porta:  ${port}`)
-})
+app.listen(port, () => {
+  // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
+
+// . mudar no notion para colocar a vercel
+// . colocar a configuração pra usar a url de conexão do post
